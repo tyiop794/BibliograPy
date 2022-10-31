8da7d221b712fc469c1bf4a7c58f444afd31a47f
tyiop794 <kamilyousuf0712@gmail.com>
2022-10-31 18:58:05 -0500

    Begin working on movie citation support

--- video.py
+++ video.py
@@ -1,8 +1,14 @@
 """
 Video citation support
 """
+from other_funcs import author_split
 
 
-class Video:
-    def __init__(self):
-        pass
+class Movie:
+    def __init__(self, title, directors, performers, distributor, year):
+        self.title = title
+        self.directors = author_split(directors)
+        self.performers = author_split(performers)
+        self.distributor = distributor
+        self.year = year
+
